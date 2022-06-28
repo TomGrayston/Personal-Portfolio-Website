61fd0d01aa59826de2b3243a3016b2998a0b76c7
Tom Grayston <tomgrayston@gmail.com>
2022-06-28 20:36:59 +0100

    fixed image sizes, spelling and added new techCon's

--- components/LandingPage/KnownTechSection/Sections/FrameworksAndTools.jsx
+++ components/LandingPage/KnownTechSection/Sections/FrameworksAndTools.jsx
@@ -1,19 +1,21 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import TechContainer from "./TechContainer";
-import firebase from "../../../../public/firebase.png";
 import react from "../../../../public/react.png";
 import next from "../../../../public/next-js.png";
 import node from "../../../../public/node-js.png";
 import unity from "../../../../public/unity.png";
+import storybook from "../../../../public/storybook.png";
+import expo from "../../../../public/expo.png";
 
     const Frameworks = ({}) => {
     return (
-        <SimpleGrid minChildWidth="100px" spacing="30px">
+        <SimpleGrid minChildWidth="95px" spacing="20px">
             <TechContainer image={react} text="React"/>
             <TechContainer image={next} text="NextJS"/>
             <TechContainer image={node} text="NodeJS"/>
+            <TechContainer image={storybook} text="Storybook"/>
+            <TechContainer image={expo} text="Expo"/>
             <TechContainer image={unity} text="Unity"/>
-            <TechContainer image={firebase} text="Firebase"/>
         </SimpleGrid>
     );
     };
--- components/LandingPage/KnownTechSection/Sections/Languages.jsx
+++ components/LandingPage/KnownTechSection/Sections/Languages.jsx
@@ -3,15 +3,17 @@ import TechContainer from "./TechContainer";
 import js from "../../../../public/js.png";
 import ts from "../../../../public/ts.png";
 import html from "../../../../public/html-css.png";
+import css3 from "../../../../public/css3.png"
 import python from "../../../../public/python.png";
 import java from "../../../../public/java.png";
 
 const Languages = ({}) => {
     return (
-        <SimpleGrid minChildWidth="100px" spacing="30px">
+        <SimpleGrid minChildWidth="95px" spacing="20px">
             <TechContainer image={js} text="JavaScript"/>
             <TechContainer image={ts} text="TypeScript"/>
-            <TechContainer image={html} text="HTML5/ CSS3"/>
+            <TechContainer image={html} text="HTML5"/>
+            <TechContainer image={css3} text="CSS3"/>
             <TechContainer image={python} text="Python"/>
             <TechContainer image={java} text="Java"/>
         </SimpleGrid>
