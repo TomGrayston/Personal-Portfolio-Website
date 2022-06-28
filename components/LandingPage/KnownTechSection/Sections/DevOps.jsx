b1095179c112cfe4aec6844db278fddecec65ad1
Tom Grayston <tomgrayston@gmail.com>
2022-06-28 20:41:15 +0100

    fixed image sizes, spelling and added new techCons

--- components/LandingPage/KnownTechSection/Sections/DevOps.jsx
+++ components/LandingPage/KnownTechSection/Sections/DevOps.jsx
@@ -7,11 +7,13 @@ import git from "../../../../public/git.png";
 import docker from "../../../../public/docker.png";
 import kubernetes from "../../../../public/kubernetes.png";
 import concourse from "../../../../public/concourse.png";
+import firebase from "../../../../public/firebase.png";
 
     const DevOps = ({}) => {
     return (
-        <SimpleGrid minChildWidth="100px" spacing="30px">
+        <SimpleGrid minChildWidth="95px" spacing="20px">
             <TechContainer image={useColorModeValue(awsL, awsD)} text="AWS"/>
+            <TechContainer image={firebase} text="Firebase"/>
             <TechContainer image={git} text="Git"/>
             <TechContainer image={docker} text="Docker"/>
             <TechContainer image={kubernetes} text="Kubernetes"/>
